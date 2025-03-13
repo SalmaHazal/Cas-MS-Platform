551f590d2e78c644973249aa756c1284a4324ef5
Abdelaali114 <abdellaali2550@gmail.com>
2025-03-13 01:02:50 +0100

    nav+current+projects

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { HomePageComponent } from './components/home-page/home-page.component';
-import { SignUpComponent } from "./components/sign-up/sign-up.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [HomePageComponent, SignUpComponent],
+  imports: [HomePageComponent],
   styleUrl: './app.component.css'
 })
 export class AppComponent {
--- frontend/src/app/components/home-page/home-page.component.ts
+++ frontend/src/app/components/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CurrentProjectComponent } from "../current-project/current-project.component";
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [CurrentProjectComponent],
+  imports: [CurrentProjectComponent,  NavbarComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
