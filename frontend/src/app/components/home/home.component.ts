62bb81255d6da28c8d06fdb3c30dc803a3744c8e
Naima <naimaelmaalmi12@gmail.com>
2025-04-18 05:37:34 +0200

    Mise à jour du frontend : ajout des sections Contact, Footer et Sponsors

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CurrentProjectComponent } from '../current-project/current-project.component';
+import { SponsorsSectionComponent } from '../sponsors-section/sponsors-section.component';
+import { ContactSectionComponent } from '../contact-section/contact-section.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [CurrentProjectComponent, RouterLink],
+  imports: [CurrentProjectComponent, RouterLink, SponsorsSectionComponent, ContactSectionComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
